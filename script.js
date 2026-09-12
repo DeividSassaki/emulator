692418421ecc6807cebc1d3797d2674d2c5c914c
Deivid Sassaki <79210851+DeividSassaki@users.noreply.github.com>
2026-09-12 11:32:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -36,7 +36,12 @@ function obterSlugDaPagina() {
 }
 
 const slugAtual = obterSlugDaPagina();
-const paginaDeJogo = slugAtual !== "";
+
+const parametros = new URLSearchParams(window.location.search);
+const romPorLink = parametros.get("rom");
+
+const paginaDeJogo =
+    slugAtual !== "" || romPorLink !== "";
 
 if (paginaDeJogo) {
     mainMenu.style.display = "none";
@@ -103,11 +108,11 @@ openUrlButton.addEventListener("click", async function () {
 // ABRIR ROM PELO LINK ?rom=
 // =========================
 
-const parametros = new URLSearchParams(window.location.search);
-const romPorLink = parametros.get("rom");
-
 if (romPorLink) {
-    iniciarEmulador(romPorLink, obterNomeArquivo(romPorLink));
+    iniciarEmulador(
+        romPorLink,
+        obterNomeArquivo(romPorLink)
+    );
 }
 
 
